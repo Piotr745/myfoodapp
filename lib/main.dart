1ea2584fd465514c182b919befd9dafd4c1967c0
Piotr745 <xdcygan6@gmail.com>
2023-06-07 14:19:22 +0200

    31321321

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Katalog App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+        appBar: AppBar(
+        title: Text('Katalog'),
+    ),
+    body: const Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+    SizedBox(height: 16),
+    Center(
+    child: Text(
+    'Katalog',
+    style: TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    ),
+    ),
+    ),
+    Image(image: AssetImage('images/obraz.png')),
+    ],
+    ),
+    ),
+    );
+  }
+}
